0aa604cb427ca39731fdce9c0ad53e5162fe9b9f
Tue Nguyen <151656341+idozii@users.noreply.github.com>
2024-04-06 08:45:17 +0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame as py
+size = (500,400)
+screen = py.display.set_mode(size)
+while True:
+    for event in py.event.get():
+        if event.type == py.MOUSEBUTTONUP:
+            pos = py.mouse.get_pos()
+            col = (0,255,255)
+            py.draw.circle(screen, col, pos, 20, 5)
+    py.display.update()
